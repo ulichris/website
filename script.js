1a7011cdc020c74acb753e1554caf9254900cf3b
ulichris <62444420+ulichris@users.noreply.github.com>
2025-07-29 10:48:50 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function openImg() {
+  var image = document.getElementById('image');
+  var source = image.src;
+  window.open(source);
+}
+function openImg() {
+  var image = document.getElementById('image2');
+  var source = image.src;
+  window.open(source);
+}
